8284439f168335b46235bbf05cb3c27acfb723e5
Kai Curry <kai@sundaysenergy.com>
2014-08-04 14:00:22 -0500

    default to 50, collection switching

--- app/views/item_container.js
+++ app/views/item_container.js
@@ -25,10 +25,42 @@
     handleUserInput: function(new_state_obj) {
       if (new_state_obj.searchTxt && new_state_obj.searchTxt.length > 1) {
         new_state_obj.searchTxt = new_state_obj.searchTxt.toLowerCase();
+        new_state_obj.pageIndex = this.getInitialState().pageIndex;
       }
+      this.filterCollection(new_state_obj);
       return this.setState(new_state_obj);
     },
+    filterCollection: function(new_state) {
+      var config, pageIndex, pageSize, reset_collection;
+      reset_collection = false;
+      if (new_state) {
+        if (new_state.category && new_state.category !== this.state.category) {
+          reset_collection = true;
+        }
+      } else {
+        new_state = this.state;
+      }
+      pageSize = new_state.pageSize || this.state.pageSize;
+      pageIndex = new_state.pageIndex || this.state.pageIndex;
+      config = {};
+      if (new_state.category) {
+        config.where = {
+          category: new_state.category
+        };
+      }
+      if (new_state.pageSize || reset_collection) {
+        config.limit = pageSize;
+      }
+      if (new_state.pageIndex || reset_collection) {
+        config.offset = pageIndex * pageSize;
+      }
+      return this.props.collection.configure(config, reset_collection);
+    },
     render: function() {
+      if (!this.isMounted()) {
+        this.filterCollection();
+        console.log('not mounted');
+      }
       return div({}, SearchBar({
         onUserInput: this.handleUserInput,
         filter: this.state
