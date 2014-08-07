35c823a050a72c23c91e427607ae9e344ecff6b7
Kai Curry <kai@sundaysenergy.com>
2014-08-06 21:07:11 -0500

    rulers for detail page.

---
+++ app/views/detail/ruler.coffee
@@ -0,0 +1,52 @@
+React = require 'react'
+{div, button, span, p, img, ul, li} = require 'reactionary'
+
+module.exports = React.createClass
+  getInitialState: ->
+    showRuler: true
+    metric: false
+
+  propTypes:
+    model: React.PropTypes.object.isRequired
+    imgSize: React.PropTypes.string.isRequired
+    cdn: React.PropTypes.string.isRequired
+
+  render: ->
+    item = @props.model
+    inchesClass = 'ruler-inches'
+    cmClass = 'ruler-cm'
+    if @state.metric
+      unit = 'cm'
+      imgClass = cmClass
+      cmClass += ' active'
+    else
+      unit = 'inch'
+      imgClass = inchesClass
+      inchesClass += ' active'
+
+    imgPath = @props.cdn+'media/ruler/'+unit+'/'+item.ruler+'-'+@props.imgSize
+
+    els = [] #elements
+    # Ruler toggle.
+    els.push ul
+      key: 'ruler-toggle'
+      className: 'ruler-toggle',
+        li
+          className: inchesClass,
+            button className: 'uppercase',
+              'Inches'
+        li
+          className: cmClass,
+            button className: 'uppercase',
+              'Centimeters'
+    # Ruler image.
+    els.push div
+      key: 'rulers'
+      className: 'rulers',
+        img:
+          className: imgClass
+          src: imgPath
+          alt: imgClass
+
+    return div className: 'ruler-wrap hidden-xs',
+      els
---
+++ app/views/detail/ruler.js
@@ -0,0 +1,64 @@
+(function() {
+  var React, button, div, img, li, p, span, ul, _ref;
+
+  React = require('react');
+
+  _ref = require('reactionary'), div = _ref.div, button = _ref.button, span = _ref.span, p = _ref.p, img = _ref.img, ul = _ref.ul, li = _ref.li;
+
+  module.exports = React.createClass({
+    getInitialState: function() {
+      return {
+        showRuler: true,
+        metric: false
+      };
+    },
+    propTypes: {
+      model: React.PropTypes.object.isRequired,
+      imgSize: React.PropTypes.string.isRequired,
+      cdn: React.PropTypes.string.isRequired
+    },
+    render: function() {
+      var cmClass, els, imgClass, imgPath, inchesClass, item, unit;
+      item = this.props.model;
+      inchesClass = 'ruler-inches';
+      cmClass = 'ruler-cm';
+      if (this.state.metric) {
+        unit = 'cm';
+        imgClass = cmClass;
+        cmClass += ' active';
+      } else {
+        unit = 'inch';
+        imgClass = inchesClass;
+        inchesClass += ' active';
+      }
+      imgPath = this.props.cdn + 'media/ruler/' + unit + '/' + item.ruler + '-' + this.props.imgSize;
+      els = [];
+      els.push(ul({
+        key: 'ruler-toggle',
+        className: 'ruler-toggle'
+      }, li({
+        className: inchesClass
+      }, button({
+        className: 'uppercase'
+      }, 'Inches')), li({
+        className: cmClass
+      }, button({
+        className: 'uppercase'
+      }, 'Centimeters'))));
+      els.push(div({
+        key: 'rulers',
+        className: 'rulers'
+      }, {
+        img: {
+          className: imgClass,
+          src: imgPath,
+          alt: imgClass
+        }
+      }));
+      return div({
+        className: 'ruler-wrap hidden-xs'
+      }, els);
+    }
+  });
+
+}).call(this);
