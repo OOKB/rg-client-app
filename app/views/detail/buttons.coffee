5584f00d16276d63f03a02a2d9119208e554289a
Kai Curry <kai@sundaysenergy.com>
2014-08-06 21:08:07 -0500

    buttons is becoming the middle section that contains a lot of state.

--- app/views/detail/buttons.coffee
+++ app/views/detail/buttons.coffee
@@ -1,26 +1,62 @@
 React = require 'react'
-{div, button, span, p} = require 'reactionary'
+{div, button, span, p, img, ul, li} = require 'reactionary'
+
+Rulers = require './ruler'
 
 module.exports = React.createClass
   getInitialState: ->
-    color_box_view: false
-    color_box_pg: 0
-    far_view: false
-    id: '01'
+    colorBoxView: false
+    colorBoxPg: 0
+    farView: false
+    pageSize: 5
+    windowWidth: window.innerWidth
+    showRuler: true
+
+  handleResize: (e) ->
+    @setState windowWidth: window.innerWidth
+
+  componentDidMount: ->
+    window.addEventListener 'resize', @handleResize
+
+  componentWillUnmount: ->
+    window.removeEventListener 'resize', @handleResize
 
   render: ->
     item = @props.model
+    cdn = '//img.rg.cape.io/'
+    # Data for the main image.
+    ww = @state.windowWidth
+    if ww < 700
+      imgSize = '640'
+    else if ww < 1800
+      imgSize = '1536'
+    else
+      imgSize = '2560'
+
+    imgPath = cdn+'items/'+item.id+'/'
+    if @state.farView
+      imgPath += 'far/'+imgSize+'.jpg'
+      imgClass = 'img-container pattern'
+      imgDiv = ''
+    else
+      imgPath += imgSize+'.jpg'
+      imgClass = 'img-container large'
+      imgDiv = img
+        className: 'img-large'
+        alt: item.name
+        src: imgPath
+
     #console.log item.toJSON()
     divs = []
 
     color_toggle_class = 'toggle-colors hidden-xs'
     if item.far
       color_toggle_class += ' with-far'
-
     divs.push div
       key: 'color-button'
       className: color_toggle_class,
         button className: 'uppercase', 'Colors'
+
     if item.far
       divs.push div
         key: 'far-button'
@@ -28,6 +64,26 @@ module.exports = React.createClass
           button className: 'uppercase',
             span className: 'pattern',
               'View Pattern'
+
+    if @state.colorBoxView
+      pages = Math.ceil(@props.collection.length / 5)
+      divs.push div
+        key: 'related-colors'
+        id: 'related-colors'
+        className: 'hidden-xs'
+
+    # big-ol-image
+    divs.push div
+      key: 'main-image'
+      className: imgPath,
+        imgDiv
+
+    # Ruler
+    if @state.showRuler and not @state.farView
+      divs.push Rulers
+        model: item
+        cdn: cdn
+        imgSize: imgSize
 
     div className: 'switcher',
       divs
--- app/views/detail/buttons.js
+++ app/views/detail/buttons.js
@@ -1,22 +1,60 @@
 (function() {
-  var React, button, div, p, span, _ref;
+  var React, Rulers, button, div, img, li, p, span, ul, _ref;
 
   React = require('react');
 
-  _ref = require('reactionary'), div = _ref.div, button = _ref.button, span = _ref.span, p = _ref.p;
+  _ref = require('reactionary'), div = _ref.div, button = _ref.button, span = _ref.span, p = _ref.p, img = _ref.img, ul = _ref.ul, li = _ref.li;
+
+  Rulers = require('./ruler');
 
   module.exports = React.createClass({
     getInitialState: function() {
       return {
-        color_box_view: false,
-        color_box_pg: 0,
-        far_view: false,
-        id: '01'
+        colorBoxView: false,
+        colorBoxPg: 0,
+        farView: false,
+        pageSize: 5,
+        windowWidth: window.innerWidth,
+        showRuler: true
       };
     },
+    handleResize: function(e) {
+      return this.setState({
+        windowWidth: window.innerWidth
+      });
+    },
+    componentDidMount: function() {
+      return window.addEventListener('resize', this.handleResize);
+    },
+    componentWillUnmount: function() {
+      return window.removeEventListener('resize', this.handleResize);
+    },
     render: function() {
-      var color_toggle_class, divs, item;
+      var cdn, color_toggle_class, divs, imgClass, imgDiv, imgPath, imgSize, item, pages, ww;
       item = this.props.model;
+      cdn = '//img.rg.cape.io/';
+      ww = this.state.windowWidth;
+      if (ww < 700) {
+        imgSize = '640';
+      } else if (ww < 1800) {
+        imgSize = '1536';
+      } else {
+        imgSize = '2560';
+      }
+      imgPath = cdn + 'items/' + item.id + '/';
+      if (this.state.farView) {
+        imgPath += 'far/' + imgSize + '.jpg';
+        imgClass = 'img-container pattern';
+        imgDiv = '';
+      } else {
+        imgPath += imgSize + '.jpg';
+        imgClass = 'img-container large';
+        imgDiv = img({
+          className: 'img-large',
+          alt: item.name,
+          src: imgPath
+        });
+      }
       divs = [];
       color_toggle_class = 'toggle-colors hidden-xs';
       if (item.far) {
@@ -37,6 +75,25 @@
         }, span({
           className: 'pattern'
         }, 'View Pattern'))));
+      }
+      if (this.state.colorBoxView) {
+        pages = Math.ceil(this.props.collection.length / 5);
+        divs.push(div({
+          key: 'related-colors',
+          id: 'related-colors',
+          className: 'hidden-xs'
+        }));
+      }
+      divs.push(div({
+        key: 'main-image',
+        className: imgPath
+      }, imgDiv));
+      if (this.state.showRuler && !this.state.farView) {
+        divs.push(Rulers({
+          model: item,
+          cdn: cdn,
+          imgSize: imgSize
+        }));
       }
       return div({
         className: 'switcher'
